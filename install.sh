1bd7ffebcb90daafc59540dea9aa98e3b98e017c
sbonnery <sbonnery@yahoo.fr>
2015-12-16 14:12:34 +0100

    test

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
+url = https://raw.githubusercontent.com/sbonnery/Test/master
+
 sudo apt-get install privoxy
-sudo bash -c "curl https://raw.githubusercontent.com/sbonnery/Test/master/etc/privoxy/config > /etc/privoxy/config"
+sudo bash -c "curl $url/etc/privoxy/config > /etc/privoxy/config"
