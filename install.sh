76e7c857bd24031d848c5599d64d8e7179224d40
sbonnery <sbonnery@yahoo.fr>
2015-12-30 21:06:13 +0100

    ajout des fichiers

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ cp etc/default/hostapd /etc/default/hostapd
 cp etc/default/ifplugd /etc/default/ifplugd
 cp etc/default/udhcpd /etc/default/udhcpd
 cp etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf
-cp /etc/network/interfaces /etc/network/interfaces
-cp /etc/privoxy/config /etc/privoxy/config
-cp /etc/sysctl.conf /etc/sysctl.conf
-cp /etc/udhcpd.conf /etc/udhcpd.conf
+cp etc/network/interfaces /etc/network/interfaces
+cp etc/privoxy/config /etc/privoxy/config
+cp etc/sysctl.conf /etc/sysctl.conf
+cp etc/udhcpd.conf /etc/udhcpd.conf
